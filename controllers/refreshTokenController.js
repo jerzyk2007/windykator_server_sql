f122aa5339180d4855bf519f76019fb65132ad90
Jurek <jerzykomorowski77@gmail.com>
2024-02-24 08:14:40 +0100

    zmieniłem czas odświeżania tokenów

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -32,7 +32,7 @@ const handleRefreshToken = async (req, res) => {
                     }
                 },
                 process.env.ACCESS_TOKEN_SECRET,
-                { expiresIn: '10s' }
+                { expiresIn: '3m' }
             );
             res.json({
                 accessToken,
