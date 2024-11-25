5c2b30584c0faa5658aee429cf2f9ae8a544b581
jerzyk2007 <jerzykomorowski77@gmail.com>
2024-11-25 14:30:31 +0100

    wyfiltrowane stringi departments sa uzależnione od tego czy dany dział jest odpowiednio dopasowany w Dopasuj Dane

--- controllers/settingsController.js
+++ controllers/settingsController.js
@@ -30,75 +30,15 @@ const changeColumns = async (req, res) => {
     await connect_SQL.query(query, values.flat());
 
 
-    // funkcja wyszukująca jakie kolumny sa przypisane do danego obszaru np area np 'SAMOCHODY NOWE"
-    // const areaDep = columns.reduce((acc, column) => {
-    //   column.areas.forEach(area => {
-    //     if (area.available) {
-    //       const existingEntry = acc.find(entry => entry.hasOwnProperty(area.name));
-    //       if (existingEntry) {
-    //         existingEntry[area.name].push({
-    //           accessorKey: column.accessorKey,
-    //           header: column.header,
-    //           filterVariant: column.filterVariant,
-    //           type: column.type,
-    //           // hide: area.hide
-    //         });
-    //       } else {
-    //         acc.push({
-    //           [area.name]: [{
-    //             accessorKey: column.accessorKey,
-    //             header: column.header,
-    //             filterVariant: column.filterVariant,
-    //             type: column.type,
-    //             // hide: area.hide
-    //           }]
-    //         });
-    //       }
-    //     }
-    //   });
-    //   return acc;
-    // }, []);
+
 
     const [userColumns] = await connect_SQL.query(
       `SELECT id_user, columns, departments, tableSettings FROM users`
     );
-    // console.log(userColumns[0].id_user);
-    // console.log(columns);
+
 
     for (const user of userColumns) {
       await verifyUserTableConfig(user.id_user, user.departments, columns);
-      // const [getUserAreas] = await connect_SQL.query(`SELECT  DISTINCT ji.area FROM users AS u LEFT JOIN join_items AS ji   ON JSON_CONTAINS(u.departments, JSON_QUOTE(ji.department), '$') WHERE id_user = '${user.id_user}'`);
-      // //  obszary(area) do jakich ma dostęp uzytkownik
-      // const areaUsers = getUserAreas.map(item => item.area);
-
-
-      // // 1. Przefiltruj areaDep, aby zostawić tylko obiekty o nazwach w areaUsers.
-      // const filteredAreas = areaDep
-      //   .filter(area =>
-      //     Object.keys(area).some(key => areaUsers.includes(key))
-      //   );
-      // // 2. Wyciągnij wszystkie obiekty z pasujących kluczy.
-      // const combinedObjects = filteredAreas.flatMap(area =>
-      //   Object.entries(area)
-      //     .filter(([key]) => areaUsers.includes(key))
-      //     .flatMap(([, values]) => values)
-      // );
-
-      // // 3. Usuń duplikaty na podstawie accessorKey.
-      // const uniqueObjects = combinedObjects.reduce((acc, obj) => {
-      //   if (!acc.some(item => item.accessorKey === obj.accessorKey)) {
-      //     acc.push(obj);
-      //   }
-      //   return acc;
-      // }, []);
-
-
-      // await connect_SQL.query(
-      //   "Update users SET columns = ? WHERE id_user = ?",
-      //   [JSON.stringify(uniqueObjects), user.id_user]
-      // );
-
-
     }
     res.end();
   } catch (error) {
@@ -110,139 +50,11 @@ const changeColumns = async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 };
-// const changeColumns = async (req, res) => {
-//   const { columns } = req.body;
-
-//   console.log(columns);
-//   try {
-//     await connect_SQL.query("TRUNCATE TABLE table_columns");
-
-//     // Teraz przygotuj dane do wstawienia
-//     const values = columns.map(item => [
-//       item.accessorKey,
-//       item.header,
-//       item.filterVariant,
-//       item.type,
-//       JSON.stringify(item.areas)
-//     ]);
-
-//     // Przygotowanie zapytania SQL z wieloma wartościami
-//     const query = `
-//           INSERT IGNORE INTO table_columns
-//             (accessorKey, header, filterVariant, type, areas) 
-//           VALUES 
-//             ${values.map(() => "(?, ?, ?, ?, ?)").join(", ")}
-//         `;
-
-//     // // Wykonanie zapytania INSERT
-//     await connect_SQL.query(query, values.flat());
-
-
-//     // funkcja wyszukująca jakie kolumny sa przypisane do danego obszaru np area np 'SAMOCHODY NOWE"
-//     const areaDep = columns.reduce((acc, column) => {
-//       column.areas.forEach(area => {
-//         if (area.available) {
-//           const existingEntry = acc.find(entry => entry.hasOwnProperty(area.name));
-//           if (existingEntry) {
-//             existingEntry[area.name].push({
-//               accessorKey: column.accessorKey,
-//               header: column.header,
-//               filterVariant: column.filterVariant,
-//               type: column.type,
-//               // hide: area.hide
-//             });
-//           } else {
-//             acc.push({
-//               [area.name]: [{
-//                 accessorKey: column.accessorKey,
-//                 header: column.header,
-//                 filterVariant: column.filterVariant,
-//                 type: column.type,
-//                 // hide: area.hide
-//               }]
-//             });
-//           }
-//         }
-//       });
-//       return acc;
-//     }, []);
-
-//     const [userColumns] = await connect_SQL.query(
-//       `SELECT id_user, columns, departments, tableSettings FROM users`
-//     );
-
-//     for (const user of userColumns) {
-//       const [getUserAreas] = await connect_SQL.query(`SELECT  DISTINCT ji.area FROM users AS u LEFT JOIN join_items AS ji   ON JSON_CONTAINS(u.departments, JSON_QUOTE(ji.department), '$') WHERE id_user = '${user.id_user}'`);
-//       //  obszary(area) do jakich ma dostęp uzytkownik
-//       const areaUsers = getUserAreas.map(item => item.area);
-
-
-//       // 1. Przefiltruj areaDep, aby zostawić tylko obiekty o nazwach w areaUsers.
-//       const filteredAreas = areaDep
-//         .filter(area =>
-//           Object.keys(area).some(key => areaUsers.includes(key))
-//         );
-//       // 2. Wyciągnij wszystkie obiekty z pasujących kluczy.
-//       const combinedObjects = filteredAreas.flatMap(area =>
-//         Object.entries(area)
-//           .filter(([key]) => areaUsers.includes(key))
-//           .flatMap(([, values]) => values)
-//       );
-
-//       // 3. Usuń duplikaty na podstawie accessorKey.
-//       const uniqueObjects = combinedObjects.reduce((acc, obj) => {
-//         if (!acc.some(item => item.accessorKey === obj.accessorKey)) {
-//           acc.push(obj);
-//         }
-//         return acc;
-//       }, []);
-
-
-//       await connect_SQL.query(
-//         "Update users SET columns = ? WHERE id_user = ?",
-//         [JSON.stringify(uniqueObjects), user.id_user]
-//       );
-
-
-
-
-//       // console.log(user.id_user);
-//       // Przechodzimy przez kolumny użytkownika wstecz, aby móc bezpiecznie usuwać elementy
-//       // for (let i = user.columns.length - 1; i >= 0; i--) {
-//       //   const userColumn = user.columns[i];
-//       //   // Sprawdzamy, czy klucz accessorKey z userColumn znajduje się w kolumnach
-//       //   const correspondingColumn = columns.find(
-//       //     (column) => column.accessorKey === userColumn.accessorKey
-//       //   );
-//       //   if (correspondingColumn) {
-//       //     // Jeśli istnieje odpowiedni obiekt w columns, podmieniamy go w user.columns
-//       //     user.columns[i] = correspondingColumn;
-//       //   } else {
-//       //     // Jeśli nie ma odpowiadającego klucza, usuwamy ten obiekt z user.columns
-//       //     user.columns.splice(i, 1);
-//       //   }
-//       // }
-
-//       // await connect_SQL.query(
-//       //   "Update users SET columns = ? WHERE id_user = ?",
-//       //   [JSON.stringify(repairColumn), user.id_user]
-//       // );
-//     }
-//     res.end();
-//   } catch (error) {
-//     logEvents(
-//       `settingsController, changeColumns: ${error}`,
-//       "reqServerErrors.txt"
-//     );
-//     console.error(error);
-//     res.status(500).json({ error: "Server error" });
-//   }
-// };
 
 //pobieranie unikalnych nazw Działów z documentów, dzięki temu jesli jakiś przybędzie/ubędzie to na Front będzie to widac w ustawieniach użytkonika
 
 
-const getFilteredDepartments = async () => {
+const getFilteredDepartments = async (res) => {
   try {
     const [mappedDepartments] = await connect_SQL.query(
       "SELECT DZIAL FROM documents"
@@ -304,11 +116,18 @@ const getSettings = async (req, res) => {
       );
     });
 
-    const uniqueDepartments = await getFilteredDepartments();
+    const uniqueDepartments = await getFilteredDepartments(res);
+
+    const [departmentsFromJI] = await connect_SQL.query(
+      "SELECT DISTINCT department FROM windykacja.join_items"
+    );
+
+    const departmentStrings = departmentsFromJI.map(item => item.department);
 
     res.json([
       { roles },
       { departments: uniqueDepartments },
+      { departmentsJI: departmentStrings },
       { columns: userSettings[0].columns },
       { permissions: userSettings[0].permissions },
     ]);
@@ -325,7 +144,7 @@ const getSettings = async (req, res) => {
 // pobieranie unikalnych nazw działów
 const getDepartments = async (req, res) => {
   try {
-    const uniqueDepartments = await getFilteredDepartments();
+    const uniqueDepartments = await getFilteredDepartments(res);
 
     //pobieram zapisane cele
     const [getTarget] = await connect_SQL.query(
