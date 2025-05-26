711b06f3e8c1d44dbd6fef099a2111b733a6b05a
jerzyk2007 <jerzykomorowski77@gmail.com>
2025-05-26 09:32:20 +0200

    przeniosłem wszytskie funkcje związane z generowaniem Raportu Fk do osobnego pliku, obecnie dane z wiekowania pobierają się z bazy MS SQL

--- routes/api/fkRaport.js
+++ routes/api/fkRaport.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const fKRaport = require("../../controllers/fkRaportController");
+const generateRaport = require("../../controllers/generateRaportFK");
 const ROLES_LIST = require("../../config/roles_list");
 const verifyRoles = require("../../middleware/verifyRoles");
 
 // pobieranie danych do raportu FK v2 wg wstępnego filtrowania
 router
   .route("/get-raport-data/:company")
-  .post(verifyRoles(ROLES_LIST.FK, ROLES_LIST.Admin), fKRaport.getRaportData);
+  .post(verifyRoles(ROLES_LIST.FK, ROLES_LIST.Admin), generateRaport.getRaportData);
 
 // pobieram daty  aktualizacji plików excel dla raportu FK
 router
   .route("/get-date-counter/:company")
-  .get(verifyRoles(ROLES_LIST.FK, ROLES_LIST.Admin), fKRaport.getDateCounter);
+  .get(verifyRoles(ROLES_LIST.FK, ROLES_LIST.Admin), generateRaport.getDateCounter);
 
 // usuwam wszystkie dane wczytanych plików excel raportu FK
 router
@@ -22,7 +23,7 @@ router
 // generowanie raportu FK wersja 2 i zapisanie w tabeli
 router
   .route("/generate-raport/:company")
-  .get(verifyRoles(ROLES_LIST.FK, ROLES_LIST.Admin), fKRaport.generateRaport);
+  .get(verifyRoles(ROLES_LIST.FK, ROLES_LIST.Admin), generateRaport.generateNewRaport);
 
 // usuwanie znacznika na wybranym dokumencie dla raportu fk 
 router
