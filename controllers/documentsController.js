2a88edadfbb8e21d815d374a1cbf438673b92c61
jerzyk2007 <jerzykomorowski77@gmail.com>
2025-12-13 11:00:41 +0100

    poprawka w zapytaniu mysql dla pobieranych danych  w tabeli

--- controllers/documentsController.js
+++ controllers/documentsController.js
@@ -1,115 +1,140 @@
 const { logEvents } = require("../middleware/logEvents");
 const { connect_SQL, msSqlQuery } = require("../config/dbConn");
 const { userProfile } = require("./manageDocumentAddition");
-const { LawPartner } = require("../config/roles_list");
 
-// const getAllDocumentsSQL1 =
-//   "SELECT IFNULL(JI.area, 'BRAK') as AREA, D.id_document, D.NUMER_FV, D.BRUTTO, D.TERMIN, D.NETTO, IFNULL(S.NALEZNOSC, 0) AS DO_ROZLICZENIA, D.DZIAL, D.DATA_FV, D.KONTRAHENT, D.DORADCA, D.NR_REJESTRACYJNY, D.NR_SZKODY, D.UWAGI_Z_FAKTURY, UPPER(D.TYP_PLATNOSCI) AS TYP_PLATNOSCI, D.NIP, D.VIN,  datediff(NOW(), D.TERMIN) AS ILE_DNI_PO_TERMINIE, ROUND((D.BRUTTO - D.NETTO), 2) AS '100_VAT', ROUND(((D.BRUTTO - D.NETTO) / 2), 2) AS '50_VAT', IF(D.TERMIN >= CURDATE(), 'N', 'P') AS CZY_PRZETERMINOWANE, D.FIRMA, IFNULL(UPPER(R.FIRMA_ZEWNETRZNA), 'BRAK') AS JAKA_KANCELARIA,  R.STATUS_AKTUALNY, DA.id_action, DA.document_id, IFNULL(DA.DZIALANIA, 'BRAK') AS DZIALANIA, IFNULL(IF(DA.KOMENTARZ_KANCELARIA_BECARED IS NOT NULL, 'KOMENTARZ ...', NULL), 'BRAK') AS KOMENTARZ_KANCELARIA_BECARED, KWOTA_WINDYKOWANA_BECARED, IFNULL(DA.NUMER_SPRAWY_BECARED, 'BRAK') AS NUMER_SPRAWY_BECARED,   IFNULL(DA.POBRANO_VAT, 'Nie dotyczy') AS POBRANO_VAT, IFNULL(UPPER(DA.STATUS_SPRAWY_KANCELARIA), 'BRAK') AS STATUS_SPRAWY_KANCELARIA, IFNULL(UPPER(DA.STATUS_SPRAWY_WINDYKACJA), 'BRAK') AS STATUS_SPRAWY_WINDYKACJA, IFNULL(DA.ZAZNACZ_KONTRAHENTA, 'NIE') AS ZAZNACZ_KONTRAHENTA,  DA.UWAGI_ASYSTENT, IFNULL(DA.BLAD_DORADCY, 'NIE') AS BLAD_DORADCY, IFNULL(DA.DATA_KOMENTARZA_BECARED, 'BRAK') AS DATA_KOMENTARZA_BECARED, DA.DATA_WYDANIA_AUTA, IFNULL(DA.OSTATECZNA_DATA_ROZLICZENIA, 'BRAK') AS OSTATECZNA_DATA_ROZLICZENIA,  IFNULL( DA.INFORMACJA_ZARZAD , 'BRAK' ) AS INFORMACJA_ZARZAD, IFNULL(DA.JAKA_KANCELARIA_TU, 'BRAK') AS JAKA_KANCELARIA_TU, DA.KRD   FROM company_documents AS D LEFT JOIN company_documents_actions AS DA ON D.id_document = DA.document_id LEFT JOIN company_settlements AS S ON D.NUMER_FV = S.NUMER_FV AND D.FIRMA = S.COMPANY LEFT JOIN company_rubicon_data AS R ON R.NUMER_FV = D.NUMER_FV LEFT JOIN company_mark_documents AS MD ON D.NUMER_FV = MD.NUMER_FV AND D.FIRMA = MD.COMPANY LEFT JOIN company_join_items AS JI ON D.DZIAL = JI.department LEFT JOIN company_fk_settlements AS FS ON D.NUMER_FV = FS.NUMER_FV AND D.FIRMA = FS.FIRMA";
-const getAllDocumentsSQL =
-  "SELECT IFNULL(JI.area, 'BRAK') as AREA, D.id_document, D.NUMER_FV, D.BRUTTO, D.TERMIN, D.NETTO, IFNULL(S.NALEZNOSC, 0) AS DO_ROZLICZENIA, D.DZIAL, D.DATA_FV, D.KONTRAHENT, D.DORADCA, D.NR_REJESTRACYJNY, D.NR_SZKODY, D.UWAGI_Z_FAKTURY, UPPER(D.TYP_PLATNOSCI) AS TYP_PLATNOSCI, D.NIP, D.VIN,  datediff(NOW(), D.TERMIN) AS ILE_DNI_PO_TERMINIE, ROUND((D.BRUTTO - D.NETTO), 2) AS '100_VAT', ROUND(((D.BRUTTO - D.NETTO) / 2), 2) AS '50_VAT', IF(D.TERMIN >= CURDATE(), 'N', 'P') AS CZY_PRZETERMINOWANE, D.FIRMA, IFNULL(UPPER(R.FIRMA_ZEWNETRZNA), 'BRAK') AS JAKA_KANCELARIA,  R.STATUS_AKTUALNY, DA.id_action, DA.document_id, IFNULL(DA.DZIALANIA, 'BRAK') AS DZIALANIA, IFNULL(IF(DA.KOMENTARZ_KANCELARIA_BECARED IS NOT NULL, 'KOMENTARZ ...', NULL), 'BRAK') AS KOMENTARZ_KANCELARIA_BECARED, KWOTA_WINDYKOWANA_BECARED, IFNULL(DA.NUMER_SPRAWY_BECARED, 'BRAK') AS NUMER_SPRAWY_BECARED,   IFNULL(DA.POBRANO_VAT, 'Nie dotyczy') AS POBRANO_VAT, IFNULL(UPPER(DA.STATUS_SPRAWY_KANCELARIA), 'BRAK') AS STATUS_SPRAWY_KANCELARIA, IFNULL(UPPER(DA.STATUS_SPRAWY_WINDYKACJA), 'BRAK') AS STATUS_SPRAWY_WINDYKACJA, IFNULL(DA.ZAZNACZ_KONTRAHENTA, 'NIE') AS ZAZNACZ_KONTRAHENTA,  DA.UWAGI_ASYSTENT, IFNULL(DA.BLAD_DORADCY, 'NIE') AS BLAD_DORADCY, IFNULL(DA.DATA_KOMENTARZA_BECARED, 'BRAK') AS DATA_KOMENTARZA_BECARED, DA.DATA_WYDANIA_AUTA, IFNULL(DA.OSTATECZNA_DATA_ROZLICZENIA, 'BRAK') AS OSTATECZNA_DATA_ROZLICZENIA,  DA.INFORMACJA_ZARZAD, IFNULL(DA.JAKA_KANCELARIA_TU, 'BRAK') AS JAKA_KANCELARIA_TU, DA.KRD   FROM company_documents AS D LEFT JOIN company_documents_actions AS DA ON D.id_document = DA.document_id LEFT JOIN company_settlements AS S ON D.NUMER_FV = S.NUMER_FV AND D.FIRMA = S.COMPANY LEFT JOIN company_rubicon_data AS R ON R.NUMER_FV = D.NUMER_FV LEFT JOIN company_mark_documents AS MD ON D.NUMER_FV = MD.NUMER_FV AND D.FIRMA = MD.COMPANY LEFT JOIN company_join_items AS JI ON D.DZIAL = JI.department LEFT JOIN company_fk_settlements AS FS ON D.NUMER_FV = FS.NUMER_FV AND D.FIRMA = FS.FIRMA";
+const getAllDocumentsSQL = `
+SELECT
+  IFNULL(JI.area, 'BRAK') AS AREA,
+  D.id_document,
+  D.NUMER_FV,
+  D.BRUTTO,
+  D.TERMIN,
+  D.NETTO,
+  IFNULL(S.NALEZNOSC, 0) AS DO_ROZLICZENIA,
+  IFNULL(FS.DO_ROZLICZENIA, 0) AS FK_DO_ROZLICZENIA,
+  D.DZIAL,
+  D.DATA_FV,
+  D.KONTRAHENT,
+  D.DORADCA,
+  D.NR_REJESTRACYJNY,
+  D.NR_SZKODY,
+  D.UWAGI_Z_FAKTURY,
+  UPPER(D.TYP_PLATNOSCI) AS TYP_PLATNOSCI,
+  D.NIP,
+  D.VIN,
+  DATEDIFF(NOW(), D.TERMIN) AS ILE_DNI_PO_TERMINIE,
+  ROUND((D.BRUTTO - D.NETTO), 2) AS VAT_100,
+  ROUND(((D.BRUTTO - D.NETTO) / 2), 2) AS VAT_50,
+  IF(D.TERMIN >= CURDATE(), 'N', 'P') AS CZY_PRZETERMINOWANE,
+  D.FIRMA,
+  IFNULL(UPPER(R.FIRMA_ZEWNETRZNA), 'BRAK') AS JAKA_KANCELARIA,
+  R.STATUS_AKTUALNY,
+  DA.id_action,
+  DA.document_id,
+  IFNULL(DA.DZIALANIA, 'BRAK') AS DZIALANIA,
+  IFNULL(
+    IF(DA.KOMENTARZ_KANCELARIA_BECARED IS NOT NULL, 'KOMENTARZ ...', NULL),
+    'BRAK'
+  ) AS KOMENTARZ_KANCELARIA_BECARED,
+  DA.KWOTA_WINDYKOWANA_BECARED,
+  IFNULL(DA.NUMER_SPRAWY_BECARED, 'BRAK') AS NUMER_SPRAWY_BECARED,
+  IFNULL(DA.POBRANO_VAT, 'Nie dotyczy') AS POBRANO_VAT,
+  IFNULL(UPPER(DA.STATUS_SPRAWY_KANCELARIA), 'BRAK') AS STATUS_SPRAWY_KANCELARIA,
+  IFNULL(UPPER(DA.STATUS_SPRAWY_WINDYKACJA), 'BRAK') AS STATUS_SPRAWY_WINDYKACJA,
+  IFNULL(DA.ZAZNACZ_KONTRAHENTA, 'NIE') AS ZAZNACZ_KONTRAHENTA,
+  DA.UWAGI_ASYSTENT,
+  IFNULL(DA.BLAD_DORADCY, 'NIE') AS BLAD_DORADCY,
+  IFNULL(DA.DATA_KOMENTARZA_BECARED, 'BRAK') AS DATA_KOMENTARZA_BECARED,
+  DA.DATA_WYDANIA_AUTA,
+  IFNULL(DA.OSTATECZNA_DATA_ROZLICZENIA, 'BRAK') AS OSTATECZNA_DATA_ROZLICZENIA,
+  DA.INFORMACJA_ZARZAD,
+  IFNULL(DA.JAKA_KANCELARIA_TU, 'BRAK') AS JAKA_KANCELARIA_TU,
+  DA.KRD
+FROM company_documents AS D
+LEFT JOIN company_documents_actions AS DA
+  ON D.id_document = DA.document_id
+LEFT JOIN company_settlements AS S
+  ON D.NUMER_FV = S.NUMER_FV AND D.FIRMA = S.COMPANY
+LEFT JOIN company_fk_settlements AS FS
+  ON D.NUMER_FV = FS.NUMER_FV AND D.FIRMA = FS.FIRMA
+LEFT JOIN company_rubicon_data AS R
+  ON R.NUMER_FV = D.NUMER_FV
+LEFT JOIN company_mark_documents AS MD
+  ON D.NUMER_FV = MD.NUMER_FV AND D.FIRMA = MD.COMPANY
+LEFT JOIN company_join_items AS JI
+  ON D.DZIAL = JI.department
+`;
 
 //pobiera faktury wg upranień uzytkownika z uwględnienień actual/archive/all SQL
 const getDataDocuments = async (id_user, info, profile) => {
-  let filteredData = [];
   const userType = userProfile(profile);
+
   try {
     const [findUser] = await connect_SQL.query(
-      "SELECT  username, usersurname, departments FROM company_users WHERE id_user = ?",
+      "SELECT departments FROM company_users WHERE id_user = ?",
       [id_user]
     );
-    const { departments = {} } = findUser[0] || {};
 
-    // jeśli użytkownik nie ma nadanych dostępów do działów to zwraca puste dane
+    const { departments = {} } = findUser[0] || {};
     const allDepartments = departments[userType] || [];
 
     if (!allDepartments.length) {
       return { data: [] };
     }
-    // dopisuje do zapytania dostęp tylko do działow zadeklarowanych
-    const sqlCondition =
-      allDepartments?.length > 0
-        ? `(${allDepartments
-            .map(
-              (dep) =>
-                `D.DZIAL = '${dep.department}' AND D.FIRMA ='${dep.company}' `
-            )
-            .join(" OR ")})`
-        : null;
 
-    if (userType === "Pracownik") {
-      if (info === "actual") {
-        [filteredData] = await connect_SQL.query(
-          `${getAllDocumentsSQL} WHERE IFNULL(S.NALEZNOSC, 0) > 0 AND ${sqlCondition}`
-        );
-      } else if (info === "critical") {
-        [filteredData] = await connect_SQL.query(
-          `${getAllDocumentsSQL} WHERE IFNULL(S.NALEZNOSC, 0) > 0 AND DATEDIFF(NOW(), D.TERMIN) >= -3  AND R.FIRMA_ZEWNETRZNA IS NULL AND DA.JAKA_KANCELARIA_TU IS NULL AND (DA.DZIALANIA != 'WINDYKACJA WEWNĘTRZNA' OR DA.DZIALANIA IS NULL)  AND ${sqlCondition}`
-        );
-      } else if (info === "obligations") {
-        [filteredData] = await connect_SQL.query(
-          `${getAllDocumentsSQL} WHERE IFNULL(S.NALEZNOSC, 0) < 0 AND ${sqlCondition}`
-        );
-      } else if (info === "archive") {
-        [filteredData] = await connect_SQL.query(
-          `${getAllDocumentsSQL} WHERE IFNULL(S.NALEZNOSC, 0) = 0 AND ${sqlCondition}`
-        );
-      } else if (info === "all") {
-        [filteredData] = await connect_SQL.query(
-          `${getAllDocumentsSQL} WHERE ${sqlCondition}`
-        );
-      } else if (info === "raport_fk") {
-        [filteredData] = await connect_SQL.query(
-          `${getAllDocumentsSQL} WHERE MD.RAPORT_FK = 1 AND ${sqlCondition}`
-        );
-      } else if (info === "disabled_fk") {
-        [filteredData] = await connect_SQL.query(
-          `${getAllDocumentsSQL} WHERE MD.RAPORT_FK = 0 AND ${sqlCondition}`
-        );
-      } else if (info === "control-bl") {
-        [filteredData] = await connect_SQL.query(
-          `${getAllDocumentsSQL} WHERE JI.area = 'BLACHARNIA' AND  ${sqlCondition} AND S.NALEZNOSC > 0 AND DA.JAKA_KANCELARIA_TU IS NULL AND R.FIRMA_ZEWNETRZNA IS NULL AND D.TERMIN < DATE_SUB(CURDATE(), INTERVAL 7 DAY) AND (DA.DZIALANIA != 'WINDYKACJA WEWNĘTRZNA' OR DA.DZIALANIA IS NULL) `
-        );
-      } else if (info === "krd") {
-        [filteredData] = await connect_SQL.query(
-          `${getAllDocumentsSQL} WHERE  ${sqlCondition} AND DA.KRD IS NOT NULL`
-        );
-      }
-      // do pobierania dla raportu róznic AS - FK
-      else if (info === "different") {
-        const getAllDocumentsSQL =
-          "SELECT IFNULL(JI.area, 'BRAK') as AREA, D.id_document, D.NUMER_FV, D.BRUTTO, D.TERMIN, D.NETTO, IFNULL(S.NALEZNOSC, 0) AS DO_ROZLICZENIA, IFNULL(FS.DO_ROZLICZENIA, 0) AS FK_DO_ROZLICZENIA,  D.DZIAL, D.DATA_FV, D.KONTRAHENT, D.DORADCA, D.NR_REJESTRACYJNY, D.NR_SZKODY, D.UWAGI_Z_FAKTURY, UPPER(D.TYP_PLATNOSCI) AS TYP_PLATNOSCI, D.NIP, D.VIN,  datediff(NOW(), D.TERMIN) AS ILE_DNI_PO_TERMINIE, ROUND((D.BRUTTO - D.NETTO), 2) AS '100_VAT', ROUND(((D.BRUTTO - D.NETTO) / 2), 2) AS '50_VAT', IF(D.TERMIN >= CURDATE(), 'N', 'P') AS CZY_PRZETERMINOWANE, D.FIRMA, IFNULL(UPPER(R.FIRMA_ZEWNETRZNA), 'BRAK') AS JAKA_KANCELARIA,  R.STATUS_AKTUALNY, DA.id_action, DA.document_id, IFNULL(DA.DZIALANIA, 'BRAK') AS DZIALANIA, IFNULL(IF(DA.KOMENTARZ_KANCELARIA_BECARED IS NOT NULL, 'KOMENTARZ ...', NULL), 'BRAK') AS KOMENTARZ_KANCELARIA_BECARED, KWOTA_WINDYKOWANA_BECARED, IFNULL(DA.NUMER_SPRAWY_BECARED, 'BRAK') AS NUMER_SPRAWY_BECARED,   IFNULL(DA.POBRANO_VAT, 'Nie dotyczy') AS POBRANO_VAT, IFNULL(UPPER(DA.STATUS_SPRAWY_KANCELARIA), 'BRAK') AS STATUS_SPRAWY_KANCELARIA, IFNULL(UPPER(DA.STATUS_SPRAWY_WINDYKACJA), 'BRAK') AS STATUS_SPRAWY_WINDYKACJA, IFNULL(DA.ZAZNACZ_KONTRAHENTA, 'NIE') AS ZAZNACZ_KONTRAHENTA,  DA.UWAGI_ASYSTENT, IFNULL(DA.BLAD_DORADCY, 'NIE') AS BLAD_DORADCY, IFNULL(DA.DATA_KOMENTARZA_BECARED, 'BRAK') AS DATA_KOMENTARZA_BECARED, DA.DATA_WYDANIA_AUTA, IFNULL(DA.OSTATECZNA_DATA_ROZLICZENIA, 'BRAK') AS OSTATECZNA_DATA_ROZLICZENIA,  IFNULL( DA.INFORMACJA_ZARZAD , 'BRAK' ) AS INFORMACJA_ZARZAD, IFNULL(DA.JAKA_KANCELARIA_TU, 'BRAK') AS JAKA_KANCELARIA_TU, DA.KRD   FROM company_documents AS D LEFT JOIN company_documents_actions AS DA ON D.id_document = DA.document_id LEFT JOIN company_settlements AS S ON D.NUMER_FV = S.NUMER_FV AND D.FIRMA = S.COMPANY LEFT JOIN company_rubicon_data AS R ON R.NUMER_FV = D.NUMER_FV LEFT JOIN company_mark_documents AS MD ON D.NUMER_FV = MD.NUMER_FV AND D.FIRMA = MD.COMPANY LEFT JOIN company_join_items AS JI ON D.DZIAL = JI.department LEFT JOIN company_fk_settlements AS FS ON D.NUMER_FV = FS.NUMER_FV AND D.FIRMA = FS.FIRMA";
+    const permissionCondition = `(${allDepartments
+      .map(
+        (dep) =>
+          `(D.DZIAL = '${dep.department}' AND D.FIRMA = '${dep.company}')`
+      )
+      .join(" OR ")})`;
 
-        [filteredData] = await connect_SQL.query(
-          `${getAllDocumentsSQL} WHERE  ${sqlCondition} AND (IFNULL(S.NALEZNOSC, 0) - IFNULL(FS.DO_ROZLICZENIA, 0)) <> 0`
-        );
-      }
-      return { data: filteredData };
-    } else {
+    const filters = {
+      actual: "AND S.NALEZNOSC > 0",
+      obligations: "AND S.NALEZNOSC < 0",
+      archive: "AND (S.NALEZNOSC = 0 OR S.NALEZNOSC IS NULL)",
+      all: "",
+      raport_fk: "AND MD.RAPORT_FK = 1",
+      disabled_fk: "AND MD.RAPORT_FK = 0",
+      krd: "AND DA.KRD IS NOT NULL",
+      critical: `
+        AND S.NALEZNOSC > 0
+        AND D.TERMIN >= DATE_SUB(NOW(), INTERVAL 3 DAY)
+        AND R.FIRMA_ZEWNETRZNA IS NULL
+        AND DA.JAKA_KANCELARIA_TU IS NULL
+        AND (DA.DZIALANIA != 'WINDYKACJA WEWNĘTRZNA' OR DA.DZIALANIA IS NULL)
+      `,
+      "control-bl": `
+        AND JI.area = 'BLACHARNIA'
+        AND S.NALEZNOSC > 0
+        AND DA.JAKA_KANCELARIA_TU IS NULL
+        AND R.FIRMA_ZEWNETRZNA IS NULL
+        AND D.TERMIN < DATE_SUB(CURDATE(), INTERVAL 7 DAY)
+        AND (DA.DZIALANIA != 'WINDYKACJA WEWNĘTRZNA' OR DA.DZIALANIA IS NULL)
+      `,
+      different: `
+        AND (IFNULL(S.NALEZNOSC, 0) - IFNULL(FS.DO_ROZLICZENIA, 0)) <> 0
+      `,
+    };
+
+    if (!filters.hasOwnProperty(info)) {
       return { data: [] };
     }
+
+    const finalQuery = `
+      ${getAllDocumentsSQL}
+      WHERE ${permissionCondition}
+      ${filters[info]}
+    `;
+
+    const [filteredData] = await connect_SQL.query(finalQuery);
+
+    return { data: filteredData };
   } catch (error) {
-    logEvents(
-      `documentsController, getDataDocuments: ${error}`,
-      "reqServerErrors.txt"
-    );
+    logEvents(`documentsController: ${error}`, "reqServerErrors.txt");
+    return { data: [] };
   }
 };
-
-// pobiera dane do tabeli w zalezności od uprawnień użytkownika, jesli nie ma pobierac rozliczonych faktur to ważne jest żeby klucz w kolekcji był DOROZLICZ_
-// const getAllDocuments = async (req, res) => {
-//   const { info, id_user } = req.params;
-//   try {
-//     const result = await getDataDocuments(id_user, info);
-
-//     res.json(result.data);
-//   } catch (error) {
-//     logEvents(
-//       `documentsController, getAllDocuments: ${error}`,
-//       "reqServerErrors.txt"
-//     );
-//     res.status(500).json({ error: "Server error" });
-//   }
-// };
 
 //SQL zmienia tylko pojedyńczy dokument, w tabeli BL po edycji wiersza
 // funkcja zmieniająca dane w poszczególnym dokumncie (editRowTable)
