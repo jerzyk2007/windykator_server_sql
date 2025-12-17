fde5401114775acff8805013f267ccd397a94b91
jerzyk2007 <jerzykomorowski77@gmail.com>
2025-12-17 07:35:27 +0100

    repairController

--- controllers/repairDataController.js
+++ controllers/repairDataController.js
@@ -42,82 +42,102 @@ const changeDocumentsTable = async () => {
     // const [tableColumns] = await connect_SQL.query(
     //   'UPDATE company_table_columns SET ACCESSOR_KEY = "KANAL_KOMUNIKACJI" WHERE ACCESSOR_KEY = "UWAGI_ASYSTENT" AND EMPLOYEE = "Pracownik"'
     // );
+    // const [userSettings] = await connect_SQL.query(
+    //   "SELECT id_user, tableSettings, columns FROM company_users"
+    // );
+    // const replaceKey = (key) =>
+    //   key === "UWAGI_ASYSTENT" ? "KANAL_KOMUNIKACJI" : key;
+    // const updateColumnsPracownik = (pracownik = []) =>
+    //   pracownik.map((item) => {
+    //     if (item.accessorKey === "UWAGI_ASYSTENT") {
+    //       return {
+    //         ...item,
+    //         accessorKey: "KANAL_KOMUNIKACJI",
+    //       };
+    //     }
+    //     return item;
+    //   });
+    // const updateTableSettingsPracownik = (pracownik) => {
+    //   if (!pracownik) return pracownik;
+    //   return {
+    //     ...pracownik,
+    //     size: Object.fromEntries(
+    //       Object.entries(pracownik.size || {}).map(([k, v]) => [
+    //         replaceKey(k),
+    //         v,
+    //       ])
+    //     ),
+    //     visible: Object.fromEntries(
+    //       Object.entries(pracownik.visible || {}).map(([k, v]) => [
+    //         replaceKey(k),
+    //         v,
+    //       ])
+    //     ),
+    //     order: (pracownik.order || []).map(replaceKey),
+    //     pinning: {
+    //       ...pracownik.pinning,
+    //       left: (pracownik.pinning?.left || []).map(replaceKey),
+    //       right: (pracownik.pinning?.right || []).map(replaceKey),
+    //     },
+    //   };
+    // };
+    // for (const user of userSettings) {
+    //   const { id_user, columns, tableSettings } = user;
+    //   // zabezpieczenie
+    //   if (!columns?.Pracownik || !tableSettings?.Pracownik) continue;
+    //   const updatedColumns = {
+    //     ...columns,
+    //     Pracownik: updateColumnsPracownik(columns.Pracownik),
+    //   };
+    //   const updatedTableSettings = {
+    //     ...tableSettings,
+    //     Pracownik: updateTableSettingsPracownik(tableSettings.Pracownik),
+    //   };
+    //   await connect_SQL.query(
+    //     `
+    //       UPDATE company_users
+    //       SET columns = ?, tableSettings = ?
+    //       WHERE id_user = ?
+    //     `,
+    //     [
+    //       JSON.stringify(updatedColumns),
+    //       JSON.stringify(updatedTableSettings),
+    //       id_user,
+    //     ]
+    //   );
+    // }
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-    const [userSettings] = await connect_SQL.query(
-      "SELECT id_user, tableSettings, columns FROM company_users"
+const changeControlBLTable = async () => {
+  try {
+    //    await connect_SQL.query(
+    //   "ALTER TABLE company_control_documents ADD COLUMN CONTROL_LOGI JSON NULL AFTER CONTROL_UWAGI"
+    // );
+
+    const [chatControlBL] = await connect_SQL.query(
+      "SELECT id_control_documents, CONTROL_UWAGI FROM company_control_documents"
     );
-    const replaceKey = (key) =>
-      key === "UWAGI_ASYSTENT" ? "KANAL_KOMUNIKACJI" : key;
 
-    const updateColumnsPracownik = (pracownik = []) =>
-      pracownik.map((item) => {
-        if (item.accessorKey === "UWAGI_ASYSTENT") {
+    for (const item of chatControlBL) {
+      if (item?.CONTROL_UWAGI?.length) {
+        const newItem = item.CONTROL_UWAGI.map((doc) => {
           return {
-            ...item,
-            accessorKey: "KANAL_KOMUNIKACJI",
+            date: "",
+            note: doc,
+            profile: "Pracownik",
+            username: "",
+            userlogin: "brak danych",
           };
-        }
-        return item;
-      });
-
-    const updateTableSettingsPracownik = (pracownik) => {
-      if (!pracownik) return pracownik;
-
-      return {
-        ...pracownik,
-
-        size: Object.fromEntries(
-          Object.entries(pracownik.size || {}).map(([k, v]) => [
-            replaceKey(k),
-            v,
-          ])
-        ),
-
-        visible: Object.fromEntries(
-          Object.entries(pracownik.visible || {}).map(([k, v]) => [
-            replaceKey(k),
-            v,
-          ])
-        ),
-
-        order: (pracownik.order || []).map(replaceKey),
-
-        pinning: {
-          ...pracownik.pinning,
-          left: (pracownik.pinning?.left || []).map(replaceKey),
-          right: (pracownik.pinning?.right || []).map(replaceKey),
-        },
-      };
-    };
-
-    for (const user of userSettings) {
-      const { id_user, columns, tableSettings } = user;
-
-      // zabezpieczenie
-      if (!columns?.Pracownik || !tableSettings?.Pracownik) continue;
-
-      const updatedColumns = {
-        ...columns,
-        Pracownik: updateColumnsPracownik(columns.Pracownik),
-      };
-
-      const updatedTableSettings = {
-        ...tableSettings,
-        Pracownik: updateTableSettingsPracownik(tableSettings.Pracownik),
-      };
-
-      await connect_SQL.query(
-        `
-          UPDATE company_users
-          SET columns = ?, tableSettings = ?
-          WHERE id_user = ?
-        `,
-        [
-          JSON.stringify(updatedColumns),
-          JSON.stringify(updatedTableSettings),
-          id_user,
-        ]
-      );
+        });
+        // console.log(newItem);
+        await connect_SQL.query(
+          "UPDATE company_control_documents SET CONTROL_UWAGI = ? WHERE id_control_documents = ?",
+          [JSON.stringify(newItem), item.id_control_documents]
+        );
+      }
     }
   } catch (error) {
     console.error(error);
@@ -128,6 +148,8 @@ const repair = async () => {
   try {
     // await changeDocumentsTable();
     // console.log("changeDocumentsTable");
+    // await changeControlBLTable();
+    // console.log("changeControlBLTable");
   } catch (error) {
     console.error(error);
   }
